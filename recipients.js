8d34ee79ff265ec0d66612f5a780ffe57631697d
LaMalisha <vitty.rimo@gmail.com>
2025-07-30 22:32:24 +0200

    Create recipients.js
    Aggiunto file recipients.js per la rubrica

---
+++ recipients.js
@@ -0,0 +1,17 @@
+// Questo file contiene i dati della rubrica dei destinatari.
+// È progettato per essere facilmente aggiornabile indipendentemente dalla logica principale dell'applicazione.
+const recipientBook = [
+    {
+        name: 'LAKEROOM',
+        address: 'VIALE DEGLI ARTIGIANI, 14',
+        city: '20832 DESIO (MB)',
+        phone: '0362 621674'
+    },
+    // Aggiungi qui altri destinatari seguendo la stessa struttura
+    // {
+    //     name: 'Nome Altro Destinatario',
+    //     address: 'Via Esempio, 10',
+    //     city: '12345 Citta (PR)',
+    //     phone: '0123 456789'
+    // }
+];
